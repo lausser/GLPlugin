8cbc2c9fccc4243aa6bb3673106c5c181b4a30a7
Gerhard Lausser <gerhard.lausser@consol.de>
2016-05-10 15:13:21 +0200

    bugfix in discover_suitable_class

--- lib/Monitoring/GLPlugin.pm
+++ lib/Monitoring/GLPlugin.pm
@@ -13,7 +13,7 @@ use Digest::MD5 qw(md5_hex);
 use Errno;
 use Data::Dumper;
 our $AUTOLOAD;
-*VERSION = \'2.0.14';
+*VERSION = \'2.0.15';
 
 use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
 
