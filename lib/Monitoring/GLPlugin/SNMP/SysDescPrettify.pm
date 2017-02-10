5ffbb5d663de872feb6f6723dc1b056537beb826
Gerhard Lausser <gerhard.lausser@consol.de>
2017-02-10 21:21:06 +0100

    move prettifier vendor patterns into glplugin

--- lib/Monitoring/GLPlugin/SNMP/SysDescPrettify.pm
+++ lib/Monitoring/GLPlugin/SNMP/SysDescPrettify.pm
@@ -3,7 +3,32 @@ our @ISA = qw(Monitoring::GLPlugin::SNMP);
 
 {
   no warnings qw(once);
-  $Monitoring::GLPlugin::SNMP::SysDescPrettify::vendor_rules = {};
+  $Monitoring::GLPlugin::SNMP::SysDescPrettify::vendor_rules = {
+    Cisco => {
+      vendor_pattern => '.*cisco.*',
+      prettifier_funcs => [
+        sub {
+          my ($sysdescr, $session) = @_;
+          if ($sysdescr =~ /(Cisco NX-OS.*? n\d+),.*(Version .*), RELEASE SOFTWARE/) {
+            return $1.' '.$2;
+          }
+          return undef;
+        },
+      ],
+    },
+    Netgear => {
+      vendor_pattern => '.*(netgear|GS\d+TP).*',
+      prettifier_funcs => [
+        sub {
+          my ($sysdescr, $session) = @_;
+          if ($sysdescr =~ /GS\d+TP/) {
+            return 'Netgear '.$sysdescr;
+          }
+          return undef;
+        },
+      ],
+    },
+  };
 }
 
 1;
