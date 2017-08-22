750f7f226d3fd2d7faf79d1aac71846b9ca8b4b1
Gerhard Lausser <gerhard.lausser@consol.de>
2017-08-22 16:42:37 +0200

    add fibre-channel-fe-mib

--- lib/Monitoring/GLPlugin.pm
+++ lib/Monitoring/GLPlugin.pm
@@ -13,7 +13,7 @@ use Digest::MD5 qw(md5_hex);
 use Errno;
 use Data::Dumper;
 our $AUTOLOAD;
-*VERSION = \'2.4.9.1';
+*VERSION = \'2.4.10';
 
 use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
 
