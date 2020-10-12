ca0aa893777d9877c1de910a8affeda3d6bc0eea
Gerhard Lausser <gerhard.lausser@consol.de>
2020-10-12 14:56:37 +0200

    add VORMETRIC-MIB and add the file as well

---
+++ lib/Monitoring/GLPlugin/SNMP/MibsAndOids/VORMETRICMIB.pm
@@ -0,0 +1,24 @@
+package Monitoring::GLPlugin::SNMP::MibsAndOids::VORMETRICMIB;
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::origin->{'VORMETRIC-MIB'} = {
+  url => '',
+  name => 'VORMETRIC-MIB',
+};
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::mib_ids->{'VORMETRIC-MIB'} =
+    '1.3.6.1.4.1.21513.1';
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'VORMETRIC-MIB'} = {
+  'vormetric' => '1.3.6.1.4.1.21513',
+  'version' => '1.3.6.1.4.1.21513.1',
+  'fingerPrint' => '1.3.6.1.4.1.21513.2',
+  'serverTime' => '1.3.6.1.4.1.21513.3',
+  'licenses' => '1.3.6.1.4.1.21513.5',
+  'serverHA' => '1.3.6.1.4.1.21513.6',
+  'diskUsage' => '1.3.6.1.4.1.21513.7',
+  'vmstat' => '1.3.6.1.4.1.21513.8',
+};
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::definitions->{'VORMETRIC-MIB'} = {
+};
+
