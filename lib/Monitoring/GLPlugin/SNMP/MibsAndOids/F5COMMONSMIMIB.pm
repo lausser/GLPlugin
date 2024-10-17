c483498f65f79a6d5a1a02cc704570ac8d93e950
Gerhard Lausser <gerhard.lausser@consol.de>
2024-10-17 19:49:05 +0200

    add F5-COMMON-SMI-MIB F5-OS-SYSTEM-MIB F5-PLATFORM-STATS-MIB

---
+++ lib/Monitoring/GLPlugin/SNMP/MibsAndOids/F5COMMONSMIMIB.pm
@@ -0,0 +1,17 @@
+package Monitoring::GLPlugin::SNMP::MibsAndOids::F5COMMONSMIMIB;
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::origin->{'F5-COMMON-SMI-MIB'} = {
+  url => '',
+  name => 'F5-COMMON-SMI-MIB',
+};
+
+#$Monitoring::GLPlugin::SNMP::MibsAndOids::mib_ids->{'F5-COMMON-SMI-MIB'} =
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'F5-COMMON-SMI-MIB'} = {
+  'f5networks' => '1.3.6.1.4.1.12276',
+  'platform' => '1.3.6.1.4.1.12276.1',
+  'f5Compliance' => '1.3.6.1.4.1.12276.2',
+};
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::definitions->{'F5-COMMON-SMI-MIB'} = {
+};
---
+++ lib/Monitoring/GLPlugin/SNMP/MibsAndOids/F5OSSYSTEMMIB.pm
@@ -0,0 +1,26 @@
+package Monitoring::GLPlugin::SNMP::MibsAndOids::F5OSSYSTEMMIB;
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::origin->{'F5-OS-SYSTEM-MIB'} = {
+  url => '',
+  name => 'F5-OS-SYSTEM-MIB',
+};
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::mib_ids->{'F5-OS-SYSTEM-MIB'} =
+  '1.3.6.1.4.1.12276.1.3';
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'F5-OS-SYSTEM-MIB'} = {
+  'f5OsSystem' => '1.3.6.1.4.1.12276.1.3',
+  'f5OsSystemModelOIDs' => '1.3.6.1.4.1.12276.1.3.1',
+  'f5OsAppR5x00' => '1.3.6.1.4.1.12276.1.3.1.1',
+  'f5OsAppR10x00' => '1.3.6.1.4.1.12276.1.3.1.2',
+  'f5OsAppR2x00' => '1.3.6.1.4.1.12276.1.3.1.3',
+  'f5OsAppR4x00' => '1.3.6.1.4.1.12276.1.3.1.4',
+  'f5OsVelosCx410' => '1.3.6.1.4.1.12276.1.3.1.5',
+  'f5OsVelosCx410Partition' => '1.3.6.1.4.1.12276.1.3.1.6',
+  'f5OsAppR12x00' => '1.3.6.1.4.1.12276.1.3.1.7',
+  'f5OsAppR5x00-DF' => '1.3.6.1.4.1.12276.1.3.1.8',
+  'f5OsAppR10x00-DF' => '1.3.6.1.4.1.12276.1.3.1.9',
+};
+
+$Monitoring::GLPlugin::SNMP::MibsAndOids::definitions->{'F5-OS-SYSTEM-MIB'} = {
+};
