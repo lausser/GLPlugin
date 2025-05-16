bc94a94db78f102f2115b27aaf85bf57a73123dc
Gerhard Lausser <gerhard.lausser@consol.de>
2025-05-16 11:11:43 +0200

    fix a bug in PowerNet-MIB, upsBasicStateOutputState is parsed again

--- lib/Monitoring/GLPlugin.pm
+++ lib/Monitoring/GLPlugin.pm
@@ -23,7 +23,7 @@ eval {
   $Data::Dumper::Sparseseen = 1;
 };
 our $AUTOLOAD;
-*VERSION = \'5.42.0.2';
+*VERSION = \'5.42.0.3';
 
 use constant { OK => 0, WARNING => 1, CRITICAL => 2, UNKNOWN => 3 };
 
