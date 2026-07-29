936e8c208af1b3647ecaad8fe9a1d35615ece2f6
Etienne Adam <etienne.adam@withings.com>
2026-07-29 19:13:54 +0200

    update/fix FS (Fiberstore) products MIBs

--- lib/Monitoring/GLPlugin/SNMP/MibsAndOids/FIBERSTOREPRODUCTSMIB.pm
+++ lib/Monitoring/GLPlugin/SNMP/MibsAndOids/FIBERSTOREPRODUCTSMIB.pm
@@ -6,24 +6,64 @@ $Monitoring::GLPlugin::SNMP::MibsAndOids::origin->{'FIBERSTORE-PRODUCTS-MIB'} = 
 };
 
 $Monitoring::GLPlugin::SNMP::MibsAndOids::mib_ids->{'FIBERSTORE-PRODUCTS-MIB'} =
-    '1.3.6.1.4.1.52642.1.1';
+    '1.3.6.1.4.1.52642.1';
 
 $Monitoring::GLPlugin::SNMP::MibsAndOids::mibs_and_oids->{'FIBERSTORE-PRODUCTS-MIB'} = {
     fsSystemInfo => '1.3.6.1.4.1.52642.1.1.1',
     fsMemTotal => '1.3.6.1.4.1.52642.1.1.1.5',
-    fsMemUsed => '1.3.6.1.4.1.52642.1.1.1.11',
-    fsMemFree => '1.3.6.1.4.1.52642.1.1.1.12',
+    fsMemFree => '1.3.6.1.4.1.52642.1.1.1.11',
+    fsMemUsed => '1.3.6.1.4.1.52642.1.1.1.12',
     fsMemUsagePct => '1.3.6.1.4.1.52642.1.1.1.13.1.5',
     fsMemWarnThreshold => '1.3.6.1.4.1.52642.1.1.1.13.1.6',
     fsMemCritThreshold => '1.3.6.1.4.1.52642.1.1.1.13.1.7',
-    fsCpuTable => '1.3.6.1.4.1.52642.1.1.2',
-    fsCpuEntry => '1.3.6.1.4.1.52642.1.1.2.1',
-    fsCpuCount => '1.3.6.1.4.1.52642.1.1.2.1.0',
-    fsCpuUsage => '1.3.6.1.4.1.52642.1.1.2.11',
+    fsCpuTable => '1.3.6.1.4.1.52642.1.1.9.13',
+    fsCpuEntry => '1.3.6.1.4.1.52642.1.1.9.13.1',
+    fsCpuUsage => '1.3.6.1.4.1.52642.1.1.9.13.1.2.0',
     fsDiskTable => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1',
     fsDiskEntry => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1.1',
     fsDiskTotal => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1.2',
-    fsDiskUsed => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1.3',
+    fsDiskFree => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1.3',
     fsDiskMount => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1.4',
     fsDiskDescr => '1.3.6.1.4.1.52642.1.1.5.1.1.1.1.6',
+
+    # ---- S5850 (FSOS-DC) environmental extensions ----
+    # Reverse-engineered from SNMP walks
+    # Fan tray / rotor table, indexed as .<col>.1.<tray>.<rotor>
+    fsFanTable         => '1.3.6.1.4.1.52642.1.37.1.1.1.1',
+    fsFanEntry         => '1.3.6.1.4.1.52642.1.37.1.1.1.1',
+    fsFanPresent       => '1.3.6.1.4.1.52642.1.37.1.1.1.1.1',
+    fsFanTrayIndex     => '1.3.6.1.4.1.52642.1.37.1.1.1.1.2',
+    fsFanRotorIndex    => '1.3.6.1.4.1.52642.1.37.1.1.1.1.3',
+    fsFanStatus        => '1.3.6.1.4.1.52642.1.37.1.1.1.1.4',
+    fsFanStatusDefinition => {
+        '1' => 'ok',
+        '2' => 'fault',
+    },
+    fsFanSpeedPct      => '1.3.6.1.4.1.52642.1.37.1.1.1.1.5',
+
+    # PSU table, indexed as .<col>.<psuIndex>
+    fsPsuTable    => '1.3.6.1.4.1.52642.1.37.1.2.1',
+    fsPsuEntry    => '1.3.6.1.4.1.52642.1.37.1.2.1',
+    fsPsuPresent  => '1.3.6.1.4.1.52642.1.37.1.2.1.3',
+    fsPsuPresentDefinition => {
+        '1' => 'present',
+        '2' => 'absent',
+    },
+    fsPsuState    => '1.3.6.1.4.1.52642.1.37.1.2.1.4',
+    fsPsuStateDefinition => {
+        '1' => 'ok',
+        '3' => 'fault',
+    },
+    fsPsuPowered  => '1.3.6.1.4.1.52642.1.37.1.2.1.7',
+    fsPsuPoweredDefinition => {
+        '1' => 'powered',
+        '2' => 'noPower',
+    },
+    fsPsuRpm      => '1.3.6.1.4.1.52642.1.37.1.2.1.9',
+
+    # Temperature sensor table (first sensor group)
+    fsTempTable    => '1.3.6.1.4.1.52642.1.90.100.1.1',
+    fsTempEntry    => '1.3.6.1.4.1.52642.1.90.100.1.1',
+    fsTempCurrent  => '1.3.6.1.4.1.52642.1.90.100.1.1.4',
+    fsTempPeak     => '1.3.6.1.4.1.52642.1.90.100.1.1.5',
 };
